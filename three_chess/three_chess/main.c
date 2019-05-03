1298fd02ffefa9bcbfca41047deafde7d038bafe
maymaying <1900750175@qq.com>
2019-05-03 13:17:07 +0800

    three chess

---
+++ three_chess/three_chess/main.c
@@ -0,0 +1,26 @@
+#include "three_chess.h"
+
+int main()
+{
+	int select = 0;
+	int quit = 0;
+	while (!quit){
+		ShowUI();
+		scanf("%d", &select);
+		switch (select){
+		case 1:
+			Game();
+			break;
+		case 2:
+			quit = 1;
+			printf("Bye,Bye!\n");
+			break;
+		default:
+			printf("Please Enter Again!\n");
+			break;
+		}
+	}
+
+	system("pause");
+	return 0;
+}
